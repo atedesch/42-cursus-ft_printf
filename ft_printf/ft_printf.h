00bc448ab576eec7a971b477128f9a001cdde95c
atedesch <andrea.a.tedeschi@gmail.com>
2024-02-16 21:59:12 +0100

    v01-01

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atedesch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 21:41:17 by atedesch          #+#    #+#             */
+/*   Updated: 2024/02/16 21:58:27 by atedesch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+# ifdef __APPLE__
+#  define NIL_STRING "0x0"
+# endif
+# ifdef __unix__
+#  define NIL_STRING "(nil)"
+# endif
+
+void	ft_putchar(char c);
+int		ft_putstr(char *s);
+char	*ft_itoa(int n);
+
+int		ft_printf(const char *str, ...);
+int		ft_putnbr(int n);
+int		ft_puthex(unsigned int n, char c);
+int		ft_putunsigned(unsigned int n);
+int		ft_putpointer(unsigned long long n);
+
+#endif

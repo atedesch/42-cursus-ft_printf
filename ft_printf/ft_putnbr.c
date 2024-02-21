2fb69196019aabaa7e0ef9c13a9ba594c63a75ac
atedesch <andrea.a.tedeschi@gmail.com>
2024-02-21 19:23:31 +0100

    v03-08

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -1,13 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putpointer.c                                    :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atedesch <atedesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/21 18:16:05 by atedesch          #+#    #+#             */
-/*   Updated: 2024/02/21 19:13:01 by atedesch         ###   ########.fr       */
+/*   Created: 2024/02/21 19:12:53 by atedesch          #+#    #+#             */
+/*   Updated: 2024/02/21 19:16:38 by atedesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_putnbr(int n)
+{
+	char	*char_n;
+	int		len;
+
+	char_n = ft_itoa(n);
+	len = ft_putstr(char_n);
+	free(char_n);
+	return (len);
+}
--- ft_printf/ft_test_main.c
+++ ft_printf/ft_test_main.c
@@ -6,19 +6,25 @@
 /*   By: atedesch <atedesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 22:20:47 by atedesch          #+#    #+#             */
-/*   Updated: 2024/02/21 17:58:10 by atedesch         ###   ########.fr       */
+/*   Updated: 2024/02/21 19:22:58 by atedesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int main(void)
+int	main(void)
 {
-	int numero = 42;
-	char carattere = 'a';
-	char *stringa = "Ciao mondo!";
-	
-	ft_printf("Il numero è: %d\n", numero);
+	int		numero;
+	int		numero2;
+	char	carattere;
+	char	*stringa;
+
+	numero = 42;
+	numero2 = 67;
+	carattere = 'a';
+	stringa = "Ciao mondo!";
+	ft_printf("Il numero (con i) è: %i\n", numero);
+	ft_printf("Il numero (con d) è: %d\n", numero2);
 	ft_printf("Il carattere è: %c\n", carattere);
 	ft_printf("La stringa è: %s\n", stringa);
 	ft_printf("Hexadecimale: %x\n", numero);

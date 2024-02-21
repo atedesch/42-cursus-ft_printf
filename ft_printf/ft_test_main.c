2246acea31a559e8c5cec42e162cb6825f1abba2
atedesch <andrea.a.tedeschi@gmail.com>
2024-02-21 19:31:19 +0100

    v03-09

--- ft_printf/ft_test_main.c
+++ ft_printf/ft_test_main.c
@@ -6,7 +6,7 @@
 /*   By: atedesch <atedesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 22:20:47 by atedesch          #+#    #+#             */
-/*   Updated: 2024/02/21 19:22:58 by atedesch         ###   ########.fr       */
+/*   Updated: 2024/02/21 19:29:54 by atedesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,21 @@ int	main(void)
 {
 	int		numero;
 	int		numero2;
+	int		num;
 	char	carattere;
 	char	*stringa;
 
 	numero = 42;
 	numero2 = 67;
+	num = 42;
 	carattere = 'a';
 	stringa = "Ciao mondo!";
 	ft_printf("Il numero (con i) è: %i\n", numero);
 	ft_printf("Il numero (con d) è: %d\n", numero2);
+	ft_printf("Conversione %%d: %d\n", num);
+	ft_printf("Conversione %%i: %i\n", num);
+	ft_printf("Conversione %%i con prefisso 0x: %i\n", 0x42);
+	ft_printf("Conversione %%i con prefisso 0: %i\n", 052);
 	ft_printf("Il carattere è: %c\n", carattere);
 	ft_printf("La stringa è: %s\n", stringa);
 	ft_printf("Hexadecimale: %x\n", numero);
